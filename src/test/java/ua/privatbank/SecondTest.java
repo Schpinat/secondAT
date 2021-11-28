921c654478c21177d843e5c35623a1723910f7a3
Stanislav Derkach <Sderkach1987@gmail.com>
2021-11-28 21:11:49 +0200

    init commit

--- src/test/java/ua/privatbank/SecondTest.java
+++ src/test/java/ua/privatbank/SecondTest.java
@@ -1,5 +1,8 @@
 package ua.privatbank;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -13,29 +16,36 @@ public class SecondTest {
     @Test
     public void secondTest () throws InterruptedException {
         // Инициализируем веб-драйвер
-        System.setProperty("webdriver.chrome.driver","D:\\JAVA\\chromedriver_win32\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        // Заходим на страницу
-        driver.get("https://privatbank.ua/ru");
-        // Ожидаем загрузки
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[@class=\"col-xs-6\"]")));
-        //Вытягиваем курс продажи доллара
-        String cursSale = (driver.findElement(By.xpath("//td[@id=\"USD_sell\"]")).getText());
-        //Очищаем поле ввода конвертера
-        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).clear();
-        //Вставляем курс продажи
-        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).sendKeys(cursSale);
-        //Ожидаем конвертации
-        Thread.sleep(3000);
-        //Вытягивае итоговое значение конвертации
+        WebDriverManager.chromedriver().setup();
+        Configuration.browser = "chrome";
+        Configuration.driverManagerEnabled = true;
+        Configuration.headless = false;
 
-       // driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
-        String cursResult =  driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
-        System.out.println("curs from vidjet is " + cursResult);
-        //Проверяем итоговое значение конвертации
-        String etalonCursResult = "1";
-        System.out.println("etalon curs" + etalonCursResult);
+        Selenide.open("https://privatbank.ua/ru");
+
+//        System.setProperty("webdriver.chrome.driver","D:\\JAVA\\chromedriver_win32\\chromedriver.exe");
+//        ChromeDriver driver = new ChromeDriver();
+//        // Заходим на страницу
+// //       driver.get("https://privatbank.ua/ru");
+//        // Ожидаем загрузки
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[@class=\"col-xs-6\"]")));
+//        //Вытягиваем курс продажи доллара
+//        String cursSale = (driver.findElement(By.xpath("//td[@id=\"USD_sell\"]")).getText());
+//        //Очищаем поле ввода конвертера
+//        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).clear();
+//        //Вставляем курс продажи
+//        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).sendKeys(cursSale);
+//        //Ожидаем конвертации
+//        Thread.sleep(3000);
+//        //Вытягивае итоговое значение конвертации
+//
+//       // driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
+//        String cursResult =  driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
+//        System.out.println("curs from vidjet is " + cursResult);
+//        //Проверяем итоговое значение конвертации
+//        String etalonCursResult = "1";
+//        System.out.println("etalon curs" + etalonCursResult);
 
        // Assert.assertEquals(cursResult,etalonCursResult);
 
