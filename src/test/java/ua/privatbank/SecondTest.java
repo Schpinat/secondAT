46f6b71d7d8d2c31ebf15944e30e26c0052f5f4f
Stanislav Derkach <Sderkach1987@gmail.com>
2021-11-28 16:03:21 +0200

    Initial commit

---
+++ src/test/java/ua/privatbank/SecondTest.java
@@ -0,0 +1,48 @@
+package ua.privatbank;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SecondTest {
+    @Test
+    public void secondTest () throws InterruptedException {
+        // Инициализируем веб-драйвер
+        System.setProperty("webdriver.chrome.driver","D:\\JAVA\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        // Заходим на страницу
+        driver.get("https://privatbank.ua/ru");
+        // Ожидаем загрузки
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[@class=\"col-xs-6\"]")));
+        //Вытягиваем курс продажи доллара
+        String cursSale = (driver.findElement(By.xpath("//td[@id=\"USD_sell\"]")).getText());
+        //Очищаем поле ввода конвертера
+        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).clear();
+        //Вставляем курс продажи
+        driver.findElement(By.xpath("//input[@name=\"inner_currency\"]")).sendKeys(cursSale);
+        //Ожидаем конвертации
+        Thread.sleep(3000);
+        //Вытягивае итоговое значение конвертации
+
+       // driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
+        String cursResult =  driver.findElement(By.xpath("//input[@name=\"outer_currency\"]")).getText();
+        System.out.println("curs from vidjet is " + cursResult);
+        //Проверяем итоговое значение конвертации
+        String etalonCursResult = "1";
+        System.out.println("etalon curs" + etalonCursResult);
+
+       // Assert.assertEquals(cursResult,etalonCursResult);
+
+
+
+
+    }
+
+}
+
